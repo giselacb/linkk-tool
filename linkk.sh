d51be10bed8cea47aac3d48eb97ab78d6dd9f140
Giselacb <123154138+giselacb@users.noreply.github.com>
2024-10-25 14:52:42 +0200

    Update linkk.sh

--- linkk.sh
+++ linkk.sh
@@ -2,6 +2,7 @@
 
 # Define tus enlaces aquí
 declare -A links=(
+    ["notion"]="https://www.notion.so/freepikcompany/1c6796cff0cc48799671a3dd2ad2eac9"
     ["vault"]="https://hashicorp-vault.fpkmon.com/ui/vault/secrets"
     ["gmail"]="https://mail.google.com/mail/u/1/#inbox"
     ["word"]="https://www.office.com/?auth=1"
@@ -12,13 +13,23 @@ declare -A links=(
     ["openweb"]="https://openwebinars.net/academia/progreso/"
     ["calendar"]="https://calendar.google.com/calendar/u/0/r"
     ["factorial"]="https://app.factorialhr.com/dashboard"
-    ["jira"]="https://team.atlassian.com/your-work?startRedirect=true"
+    ["jira"]="https://freepikcompany.atlassian.net/jira/your-work"
 )
 
 # Define tus perfiles aquí
 declare -A profiles=(
     ["t"]="Default"
     ["p"]="Profile 5"
+)
+
+# Define tus programas aquí
+declare -A programs=(
+    ["vscode"]="code"
+    ["terminal"]="konsole"
+    ["explorer"]="nautilus"
+    ["slack"]="slack"
+    ["kate"]="kate"
+    ["dolphin"]="dolphin"
 )
 
 # Función para mostrar la ayuda
@@ -29,6 +40,10 @@ function show_help {
     echo "  list <perfil>               Lista los enlaces y permite seleccionar uno para abrir con el perfil especificado."
     echo "  add-url <nombre> <url>      Añade una nueva URL al script con el nombre especificado."
     echo "  add-profile <nombre> <perfil> Añade un nuevo perfil al script con el nombre y perfil especificados."
+    echo "  delete-url <nombre>         Elimina el enlace especificado del script."
+    echo "  close-chrome                Cierra todas las ventanas de Google Chrome."
+    echo "  run <programa> <directorio> Ejecuta un programa con el directorio de trabajo especificado."
+    echo "  list-programs               Muestra la lista de programas disponibles."
     echo "  help                        Muestra esta ayuda."
     echo ""
     echo "Abrir una o varias URLs:"
@@ -42,18 +57,19 @@ function show_help {
     echo "  $0 add-profile dev Profile 6"
     echo "                               Añade un nuevo perfil 'dev' asociado con 'Profile 6'."
     echo "  $0 vault gmail trabajo       Abre las URLs 'vault' y 'gmail' con el perfil 'trabajo'."
+    echo "  $0 delete-url notion          Elimina el enlace con nombre 'notion'."
+    echo "  $0 close-chrome              Cierra todas las ventanas de Google Chrome."
+    echo "  $0 run vscode ~/mi-proyecto  Abre VSCode en el directorio '~/mi-proyecto'."
+    echo "  $0 list-programs             Muestra la lista de programas disponibles."
     echo ""
-    echo "Requisitos:"
-    echo "  - Este script requiere 'fzf' para la funcionalidad interactiva de selección de enlaces."
-    echo "    Puedes instalarlo usando:"
-    echo "    - En Debian/Ubuntu: sudo apt install fzf"
-    echo "    - En Fedora: sudo dnf install fzf"
-    echo "    - En Arch Linux: sudo pacman -S fzf"
-    echo ""
-    echo "Cómo encontrar el nombre de tu perfil de Chrome:"
-    echo "  - Abre Google Chrome y ve a 'chrome://version/'."
-    echo "  - Busca la línea 'Profile Path' para encontrar el nombre del perfil."
-    echo ""
+}
+
+# Función para listar los programas disponibles
+function list_programs {
+    echo "Programas disponibles:"
+    for program in "${!programs[@]}"; do
+        echo "  $program -> ${programs[$program]}"
+    done
 }
 
 # Función para añadir nuevas URLs
@@ -86,6 +102,25 @@ function add_profile {
     echo "Perfil añadido: $name -> $profile"
 }
 
+# Función para eliminar URLs
+function delete_url {
+    local name="$1"
+
+    if [[ -z "$name" ]]; then
+        echo "Uso: $0 delete-url <nombre>"
+        exit 1
+    fi
+
+    if [[ -z "${links[$name]}" ]]; then
+        echo "El enlace $name no existe."
+        exit 1
+    fi
+
+    # Eliminar la URL del script
+    sed -i "/\[\"$name\"\]=/d" "$0"
+    echo "URL eliminada: $name"
+}
+
 # Función para listar todos los nombres de los enlaces y seleccionar uno
 function select_and_open_link {
     local profile_key="$1"
@@ -106,13 +141,43 @@ function select_and_open_link {
     google-chrome --profile-directory="$profile_name" "${links[$selected_link]}"
 }
 
+# Función para cerrar todas las ventanas de Google Chrome
+function close_chrome {
+    echo "Cerrando todas las ventanas de Google Chrome..."
+    pkill -f "chrome"
+}
+
+# Función para ejecutar un programa con un directorio específico
+function run_program {
+    local program="$1"
+    local directory="$2"
+
+    if [ -z "${programs[$program]}" ]; then
+        echo "Programa no encontrado: $program"
+        exit 1
+    fi
+
+    # Convertir el directorio a una ruta absoluta
+    local absolute_directory
+    absolute_directory=$(realpath "$directory" 2> /dev/null)
+
+    if [ ! -d "$absolute_directory" ]; then
+        echo "Directorio no encontrado: $directory"
+        exit 1
+    fi
+
+    echo "Abriendo ${programs[$program]} en el directorio $absolute_directory..."
+    (cd "$absolute_directory" && "${programs[$program]}") &
+}
+
+
 # Verifica si el primer parámetro es "help"
 if [ "$1" == "help" ]; then
     show_help
     exit 0
 fi
 
-# Comandos adicionales para añadir URLs y perfiles
+# Comandos adicionales para añadir y eliminar URLs y perfiles
 if [ "$1" == "add-url" ]; then
     add_url "$2" "$3"
     exit 0
@@ -123,21 +188,46 @@ if [ "$1" == "add-profile" ]; then
     exit 0
 fi
 
+if [ "$1" == "delete-url" ]; then
+    delete_url "$2"
+    exit 0
+fi
+
+# Comando para cerrar todas las ventanas de Chrome
+if [ "$1" == "close-chrome" ]; then
+    close_chrome
+    exit 0
+fi
+
 # Verifica si el primer parámetro es "list"
 if [ "$1" == "list" ]; then
     if [ $# -ne 2 ]; then
         echo "Uso: $0 list <nombre_del_perfil>"
-        echo "Ejemplo: $0 list trabajo"
         exit 1
     fi
     select_and_open_link "$2"
     exit 0
 fi
 
+# Comando para listar los programas disponibles
+if [ "$1" == "list-programs" ]; then
+    list_programs
+    exit 0
+fi
+
+# Comando para ejecutar un programa con un directorio específico
+if [ "$1" == "run" ]; then
+    if [ $# -ne 3 ]; then
+        echo "Uso: $0 run <programa> <directorio>"
+        exit 1
+    fi
+    run_program "$2" "$3"
+    exit 0
+fi
+
 # Verifica si el número de parámetros es correcto para apertura directa
 if [ $# -lt 2 ]; then
     echo "Uso: $0 <nombre_del_link> <nombre_del_perfil>"
-    echo "Ejemplo: $0 vault trabajo"
     exit 1
 fi
 
